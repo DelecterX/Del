642ff3994792cf9c36fa19b299ef16f9c609b3de
it1702046 <it1702046@02-LAB-308-03.ad.vsu>
2018-01-13 11:50:23 +0200

    init

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
 
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner console = new Scanner(System.in);
+		
+		
+		double a = a + Math.random() * (b - a);
+		double b = b + Math.random() * (a - b);
+		double c = c + Math.random() * (a - b);
+		
+		System.out.print(a);
+		System.out.print(" - ");
+		System.out.print(b);
+		System.out.print(" ÷ ");
+		System.out.print(c);
+		
+		x = console.nextInt();
 
+		
 	}
 
 }
